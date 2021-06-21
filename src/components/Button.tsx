6900e095ede76602af7281062a33c6369d264d15
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-06-21 12:47:21 -0300

    feat: example
    create basic example components, properties and state

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+// type ButtonProps = {
+//   children?: string;
+// }
+
+import { useState } from "react";
+
+export function Button() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+    console.log(counter);
+  }
+
+  return (
+    <button onClick={increment}>{counter}</button>
+  )
+}
